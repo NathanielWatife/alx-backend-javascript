18be830a4660a6a9b03ddbf8c91a43acf67530a3
NathanielWatife <awosanminathaniel0@gmail.com>
2024-10-16 07:22:06 +0100

    throw an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+export default function guardrail(mathFunction) {
+    const queue = [];
+  
+    const processResult = (result) => {
+      queue.push(result);
+      queue.push('Guardrail was processed');
+      return queue;
+    };
+  
+    try {
+      const res = mathFunction();
+      return processResult(res);
+    } catch (err) {
+      return processResult(err.toString());
+    }
+  }
