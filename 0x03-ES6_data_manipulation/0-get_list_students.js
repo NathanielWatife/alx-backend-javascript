33a4297c9616074215eca26838b639ade24b37b1
NathanielWatife <awosanminathaniel0@gmail.com>
2024-10-22 19:01:55 +0100

    Basic list of subject

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,22 @@
+function getListStudents(){
+    const students = [
+        {
+            id: 1,
+            firstname: 'Guillaume',
+            location: 'San Fransico'
+        },
+        {
+            id: 2,
+            firstname: 'James',
+            location: 'Columbia'
+        },
+        {
+            id: 5,
+            firstname: 'Serena',
+            location: 'San Fransico'
+        }
+    ];
+    return students;
+}
+
+export default getListStudents;
